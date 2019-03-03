6762b418ab2490e4c9cdf17cc4fbc6d417a82152
gbe <geoffrey.berard@lesfurets.com>
2019-03-03 21:09:18 +0100

    Adding service position

--- src/container/HelloWorld.js
+++ src/container/HelloWorld.js
@@ -1,27 +1,38 @@
 import React from "react";
-import Position, {POSITION} from "./Position";
+import Position, {ACTION, POSITION} from "./Position";
 
 export default class HelloWorld extends React.Component {
     constructor() {
         super();
         this.state = {
-            position: POSITION.P1
+            position: POSITION.P2,
+            action: ACTION.SERVING,
         };
-        this.onChange = this.onChange.bind(this);
+        this.onPositionChange = this.onPositionChange.bind(this);
+        this.onActionChange = this.onActionChange.bind(this);
     }
 
-    onChange(event) {
+    onPositionChange(event) {
         this.setState({position: event.target.value});
     }
 
+    onActionChange(event) {
+        this.setState({action: event.target.value});
+    }
+
     render() {
+        console.log(this.state);
         return (
             <div className="helo-world">
                 Hello World !
-                <select onChange={this.onChange}>
+                <select onChange={this.onPositionChange}>
                     {Object.values(POSITION).map(position => <option value={position}>{position}</option>)}
                 </select>
-                <Position position={this.state.position}/>
+                <select onChange={this.onActionChange}>
+                    {Object.values(ACTION).map(action => <option value={action}>{action}</option>)}
+                </select>
+                Actually : {this.state.position} {this.state.action}
+                <Position position={this.state.position} action={this.state.action}/>
             </div>
         );
     }
--- src/container/Position.js
+++ src/container/Position.js
@@ -7,118 +7,238 @@ import Player from "./Player";
 export const MAX_SIZE = 1100;
 
 const ORIGINAL = {
-  O1:{px:80,py:70},
-  O2:{px:80,py:20},
-  O3:{px:50,py:20},
-  O4:{px:20,py:20},
-  O5:{px:20,py:70},
-  O6:{px:50,py:70},
+    O1: {px: 80, py: 70},
+    O2: {px: 80, py: 20},
+    O3: {px: 50, py: 20},
+    O4: {px: 20, py: 20},
+    O5: {px: 20, py: 70},
+    O6: {px: 50, py: 70},
+    SERVICE: {px: 80, py: 110},
 };
 
 export const POSITION = {
-    P1:"P1",
-    P6:"P6",
-    P5:"P5",
-    P4:"P4",
-    P3:"P3",
-    P2:"P2"
-}
+    P1: "P1",
+    P6: "P6",
+    P5: "P5",
+    P4: "P4",
+    P3: "P3",
+    P2: "P2"
+};
+
+export const ACTION = {
+    ORIGINAL: "ORIGINAL",
+    SERVING: "SERVING",
+    RECEIVING: "RECEIVING"
+};
 
 const PLAYERS = {
     SETTER: {
-        id:"SETTER",
+        id: "SETTER",
         label: "Passeuse",
         highlight: "#283593",
         understate: "#9fa8da",
     },
     OPPOSITE: {
-        id:"OPPOSITE",
+        id: "OPPOSITE",
         label: "Pointe",
         highlight: "#6a1b9a",
         understate: "#ce93d8",
     },
     OUTSIDE_FRONT: {
-        id:"OUTSIDE_FRONT",
+        id: "OUTSIDE_FRONT",
         label: "Réceptionneuses-Attaquantes Avant",
         highlight: "#c62828",
         understate: "#ef9a9a",
     },
     OUTSIDE_BACK: {
-        id:"OUTSIDE_BACK",
+        id: "OUTSIDE_BACK",
         label: "Réceptionneuses-Attaquantes Arrière",
         highlight: "#c62828",
         understate: "#ef9a9a",
     },
     MIDDLE_FRONT: {
-        id:"MIDDLE_FRONT",
+        id: "MIDDLE_FRONT",
         label: "Centrale Avant",
         highlight: "#2e7d32",
         understate: "#a5d6a7",
     },
     MIDDLE_BACK: {
-        id:"MIDDLE_BACK",
+        id: "MIDDLE_BACK",
         label: "Centrale Arrière",
         highlight: "#2e7d32",
         understate: "#a5d6a7",
     }
 };
 
+let OUSTIDE_BACK_MIDDLE = {px: 50, py: 80};
+
+let MIDDLE_FRONT = {px: 50, py: 5};
 const PLAYER_POSITION = {
-    [POSITION.P1]:{
-        [PLAYERS.SETTER.id]:ORIGINAL.O1,
-        [PLAYERS.OUTSIDE_FRONT.id]:ORIGINAL.O2,
-        [PLAYERS.MIDDLE_FRONT.id]:ORIGINAL.O3,
-        [PLAYERS.OPPOSITE.id]:ORIGINAL.O4,
-        [PLAYERS.OUTSIDE_BACK.id]:ORIGINAL.O5,
-        [PLAYERS.MIDDLE_BACK.id]:ORIGINAL.O6,
+    [POSITION.P1]: {
+        [PLAYERS.SETTER.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O1,
+            [ACTION.SERVING]: ORIGINAL.SERVICE,
+        },
+        [PLAYERS.OUTSIDE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O2,
+            [ACTION.SERVING]: {px: 60, py: 15},
+        },
+        [PLAYERS.MIDDLE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O3,
+            [ACTION.SERVING]: MIDDLE_FRONT,
+        },
+        [PLAYERS.OPPOSITE.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O4,
+            [ACTION.SERVING]: {px: 40, py: 10},
+        },
+        [PLAYERS.OUTSIDE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O5,
+            [ACTION.SERVING]: OUSTIDE_BACK_MIDDLE,
+        },
+        [PLAYERS.MIDDLE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O6,
+            [ACTION.SERVING]: {px: 60, py: 70},
+        },
     },
-    [POSITION.P6]:{
-        [PLAYERS.SETTER.id]:ORIGINAL.O6,
-        [PLAYERS.OUTSIDE_FRONT.id]:ORIGINAL.O1,
-        [PLAYERS.MIDDLE_FRONT.id]:ORIGINAL.O2,
-        [PLAYERS.OPPOSITE.id]:ORIGINAL.O3,
-        [PLAYERS.OUTSIDE_BACK.id]:ORIGINAL.O4,
-        [PLAYERS.MIDDLE_BACK.id]:ORIGINAL.O5,
+    [POSITION.P6]: {
+        [PLAYERS.SETTER.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O6,
+            [ACTION.SERVING]: {px: 80, py: 70},
+        },
+        [PLAYERS.OUTSIDE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O1,
+            [ACTION.SERVING]: ORIGINAL.SERVICE,
+        },
+        [PLAYERS.MIDDLE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O2,
+            [ACTION.SERVING]: MIDDLE_FRONT,
+        },
+        [PLAYERS.OPPOSITE.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O3,
+            [ACTION.SERVING]: {px: 40, py: 10},
+        },
+        [PLAYERS.OUTSIDE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O4,
+            [ACTION.SERVING]: {px: 30, py: 5},
+        },
+        [PLAYERS.MIDDLE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O5,
+            [ACTION.SERVING]: ORIGINAL.O5,
+        },
     },
-    [POSITION.P5]:{
-        [PLAYERS.SETTER.id]:ORIGINAL.O5,
-        [PLAYERS.OUTSIDE_FRONT.id]:ORIGINAL.O6,
-        [PLAYERS.MIDDLE_FRONT.id]:ORIGINAL.O1,
-        [PLAYERS.OPPOSITE.id]:ORIGINAL.O2,
-        [PLAYERS.OUTSIDE_BACK.id]:ORIGINAL.O3,
-        [PLAYERS.MIDDLE_BACK.id]:ORIGINAL.O4,
+    [POSITION.P5]: {
+        [PLAYERS.SETTER.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O5,
+            [ACTION.SERVING]: {px: 40, py: 70},
+        },
+        [PLAYERS.OUTSIDE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O6,
+            [ACTION.SERVING]: OUSTIDE_BACK_MIDDLE,
+        },
+        [PLAYERS.MIDDLE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O1,
+            [ACTION.SERVING]: ORIGINAL.SERVICE,
+        },
+        [PLAYERS.OPPOSITE.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O2,
+            [ACTION.SERVING]: {px: 70, py: 5},
+        },
+        [PLAYERS.OUTSIDE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O3,
+            [ACTION.SERVING]: {px: 60, py: 10},
+        },
+        [PLAYERS.MIDDLE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O4,
+            [ACTION.SERVING]: MIDDLE_FRONT,
+        },
     },
-    [POSITION.P4]:{
-        [PLAYERS.SETTER.id]:ORIGINAL.O4,
-        [PLAYERS.OUTSIDE_FRONT.id]:ORIGINAL.O5,
-        [PLAYERS.MIDDLE_FRONT.id]:ORIGINAL.O6,
-        [PLAYERS.OPPOSITE.id]:ORIGINAL.O1,
-        [PLAYERS.OUTSIDE_BACK.id]:ORIGINAL.O2,
-        [PLAYERS.MIDDLE_BACK.id]:ORIGINAL.O3,
+    [POSITION.P4]: {
+        [PLAYERS.SETTER.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O4,
+            [ACTION.SERVING]: {px: 40, py: 10},
+        },
+        [PLAYERS.OUTSIDE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O5,
+            [ACTION.SERVING]: OUSTIDE_BACK_MIDDLE,
+        },
+        [PLAYERS.MIDDLE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O6,
+            [ACTION.SERVING]: {px: 60, py: 70},
+        },
+        [PLAYERS.OPPOSITE.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O1,
+            [ACTION.SERVING]: ORIGINAL.SERVICE,
+        },
+        [PLAYERS.OUTSIDE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O2,
+            [ACTION.SERVING]: {px: 60, py: 15},
+        },
+        [PLAYERS.MIDDLE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O3,
+            [ACTION.SERVING]: MIDDLE_FRONT,
+        },
     },
-    [POSITION.P3]:{
-        [PLAYERS.SETTER.id]:ORIGINAL.O3,
-        [PLAYERS.OUTSIDE_FRONT.id]:ORIGINAL.O4,
-        [PLAYERS.MIDDLE_FRONT.id]:ORIGINAL.O5,
-        [PLAYERS.OPPOSITE.id]:ORIGINAL.O6,
-        [PLAYERS.OUTSIDE_BACK.id]:ORIGINAL.O1,
-        [PLAYERS.MIDDLE_BACK.id]:ORIGINAL.O2,
+    [POSITION.P3]: {
+        [PLAYERS.SETTER.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O3,
+            [ACTION.SERVING]: {px: 40, py: 10},
+        },
+        [PLAYERS.OUTSIDE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O4,
+            [ACTION.SERVING]: {px: 30, py: 5},
+        },
+        [PLAYERS.MIDDLE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O5,
+            [ACTION.SERVING]: ORIGINAL.O5,
+        },
+        [PLAYERS.OPPOSITE.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O6,
+            [ACTION.SERVING]: {px: 80, py: 70},
+        },
+        [PLAYERS.OUTSIDE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O1,
+            [ACTION.SERVING]: ORIGINAL.SERVICE,
+        },
+        [PLAYERS.MIDDLE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O2,
+            [ACTION.SERVING]: MIDDLE_FRONT,
+        },
     },
-    [POSITION.P2]:{
-        [PLAYERS.SETTER.id]:ORIGINAL.O2,
-        [PLAYERS.OUTSIDE_FRONT.id]:ORIGINAL.O3,
-        [PLAYERS.MIDDLE_FRONT.id]:ORIGINAL.O4,
-        [PLAYERS.OPPOSITE.id]:ORIGINAL.O5,
-        [PLAYERS.OUTSIDE_BACK.id]:ORIGINAL.O6,
-        [PLAYERS.MIDDLE_BACK.id]:ORIGINAL.O1,
+    [POSITION.P2]: {
+        [PLAYERS.SETTER.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O2,
+            [ACTION.SERVING]: {px: 70, py: 5},
+        },
+        [PLAYERS.OUTSIDE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O3,
+            [ACTION.SERVING]: {px: 60, py: 10},
+        },
+        [PLAYERS.MIDDLE_FRONT.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O4,
+            [ACTION.SERVING]: MIDDLE_FRONT,
+        },
+        [PLAYERS.OPPOSITE.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O5,
+            [ACTION.SERVING]: {px: 40, py: 70},
+        },
+        [PLAYERS.OUTSIDE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O6,
+            [ACTION.SERVING]: OUSTIDE_BACK_MIDDLE,
+        },
+        [PLAYERS.MIDDLE_BACK.id]: {
+            [ACTION.ORIGINAL]: ORIGINAL.O1,
+            [ACTION.SERVING]: ORIGINAL.SERVICE,
+        },
     },
 }
 
-const Position = ({position}) => (
+const Position = ({position, action}) => (
     <svg viewBox={`0 0 ${MAX_SIZE} ${MAX_SIZE}`}>
         <Court>
             {Object.values(PLAYERS)
-                .map(player => <Player player={player} position={PLAYER_POSITION[position][player.id]}/>)}
+                .map(player => <Player
+                    player={player}
+                    position={PLAYER_POSITION[position][player.id][action]}/>)}
         </Court>
         {Array.apply(null, {length: 12})
             .map(Number.call, Number)
@@ -129,8 +249,9 @@ const Position = ({position}) => (
     </svg>
 );
 
-Position.defaultProps= {
-    position: POSITION.P1
+Position.defaultProps = {
+    position: POSITION.P1,
+    action: ACTION.ORIGINAL,
 };
 
 export default Position;
