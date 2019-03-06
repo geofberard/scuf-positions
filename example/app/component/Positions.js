50fa6729b9807011e2c06fde5f4e842d08b68275
gbe <geoffrey.berard@lesfurets.com>
2019-03-06 01:43:57 +0100

    Adding App Bar

--- example/app/component/Positions.js
+++ example/app/component/Positions.js
@@ -7,12 +7,24 @@ import {EPlayer, Position} from "../../../src";
 import {PLAYER_SCUF} from "./position-scuf";
 import {EAction, EActions} from "../data/EAction";
 import {EPosition, EPositions} from "../data/EPosition";
-// import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import "../../styles/positions.scss"
 
 const ALL_PLAYERS = {
     id: "ALL_PLAYERS",
     label: "Tous"
 };
+
+const EPlayerFilter = {
+    ALL_PLAYERS: ALL_PLAYERS,
+    ...EPlayer
+};
+
+const EPlayerFilters = Object.values(EPlayerFilter);
+
 
 const modulo = (value, n) => ((value % n) + n) % n;
 const getNextElement = (current, diff, list) => list[modulo(list.indexOf(current) + diff, list.length)];
@@ -23,7 +35,7 @@ export default class Positions extends React.Component {
         this.state = {
             position: EPosition.P1,
             action: EAction.ORIGINAL,
-            focus: ALL_PLAYERS,
+            focus: ALL_PLAYERS
         };
         this.onPositionChange = this.onPositionChange.bind(this);
         this.onPositionNavigation = this.onPositionNavigation.bind(this);
@@ -49,50 +61,60 @@ export default class Positions extends React.Component {
     }
 
     onPlayerChange(event) {
-        this.setState({focus: EPlayer[event.target.value] || ALL_PLAYERS});
+        this.setState({focus: EPlayerFilter[event.target.value]});
     }
 
     render() {
         return (
-            <Paper elevation={4}>
-                <FormControl>
-                    <InputLabel htmlFor="position-select">Position</InputLabel>
-                    <Select native
-                            value={this.state.position.id}
-                            onChange={this.onPositionChange}
-                            inputProps={{name: 'position', id: 'position-select',}}>
-                        {EPositions.map(position => <option value={position.id}>{position.label}</option>)}
-                    </Select>
-                </FormControl>
-                <div onClick={() => this.onPositionNavigation(-1)}>Previous</div>
-                <div onClick={() => this.onPositionNavigation(1)}>Next</div>
-                <br/>
-                <FormControl>
-                    <InputLabel htmlFor="action-select">Action</InputLabel>
-                    <Select native
-                            value={this.state.action.id}
-                            onChange={this.onActionChange}
-                            inputProps={{name: 'action', id: 'action-select',}}>
-                        {EActions.map(action => <option value={action.id}>{action.label}</option>)}
-                    </Select>
-                </FormControl>
-                <div onClick={() => this.onActionNavigation(-1)}>Previous</div>
-                <div onClick={() => this.onActionNavigation(1)}>Next</div>
-                <br/>
-                <FormControl>
-                    <InputLabel htmlFor="player-select">Poste</InputLabel>
-                    <Select native
-                            value={this.state.focus ? this.state.focus.id : ""}
+            <React.Fragment>
+                <AppBar className={"MenuBar"} position="fixed">
+                    <Toolbar>
+                        <Typography className={"Title"} variant="headline" color="inherit">
+                            SCUF - Positions
+                        </Typography>
+                        <Typography variant="subtitle1" color="inherit">
+                            Poste:
+                        </Typography>
+                        <Select native
+                            color="inherit"
+                            value={this.state.focus.id}
                             onChange={this.onPlayerChange}
-                            inputProps={{name: 'player', id: 'player-select',}}>
-                        <option value={ALL_PLAYERS.id}>{ALL_PLAYERS.label}</option>
-                        {Object.values(EPlayer)
-                            .map(player => <option value={player.id}>{player.label}</option>)}
-                    </Select>
-                </FormControl><br/>
-                <Position positions={PLAYER_SCUF[this.state.position.id][this.state.action.id]}
-                          focus={this.state.focus === ALL_PLAYERS ? undefined : this.state.focus}/>
-            </Paper>
+                            className={"PlayerSelect-input"}>
+                            {EPlayerFilters.map(player => (<option value={player.id}>{player.label}</option>))}
+                        </Select>
+                    </Toolbar>
+                </AppBar>
+                <div className="AppContainer">
+                    <Paper className="PositionPaper" elevation={4}>
+                        <FormControl>
+                            <InputLabel htmlFor="position-select">Position</InputLabel>
+                            <Select native
+                                    value={this.state.position.id}
+                                    onChange={this.onPositionChange}
+                                    inputProps={{name: 'position', id: 'position-select'}}>
+                                {EPositions.map(position => <option value={position.id}>{position.label}</option>)}
+                            </Select>
+                        </FormControl>
+                        <div onClick={() => this.onPositionNavigation(-1)}>Previous</div>
+                        <div onClick={() => this.onPositionNavigation(1)}>Next</div>
+                        <br/>
+                        <FormControl>
+                            <InputLabel htmlFor="action-select">Action</InputLabel>
+                            <Select native
+                                    value={this.state.action.id}
+                                    onChange={this.onActionChange}
+                                    inputProps={{name: 'action', id: 'action-select',}}>
+                                {EActions.map(action => <option value={action.id}>{action.label}</option>)}
+                            </Select>
+                        </FormControl>
+                        <div onClick={() => this.onActionNavigation(-1)}>Previous</div>
+                        <div onClick={() => this.onActionNavigation(1)}>Next</div>
+                        <br/>
+                        <Position positions={PLAYER_SCUF[this.state.position.id][this.state.action.id]}
+                                  focus={this.state.focus === ALL_PLAYERS ? undefined : this.state.focus}/>
+                    </Paper>
+                </div>
+            </React.Fragment>
         );
     }
 }
